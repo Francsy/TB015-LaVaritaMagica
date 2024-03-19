c6707883450e2f10f94dbbae9093dff4de5cd9c2
Francsy <francsyludens@gmail.com>
2024-03-19 13:11:55 +0000

    refactor/scripts.js

--- scripts.js
+++ scripts.js
@@ -1,173 +1,178 @@
-// 1. Evita el comportamiento por defecto al hacer click. Por ejemplo, si hago click sobre un enlace, este no me llevará a otra página.
-//Declaramos una variable que seleccióon el elemento html
+// 1. Prevent default behavior when clicking. For example, if I click on a link, it won't take me to another page.
+// Declare a variable that selects the HTML element.
 
 let root = document.documentElement;
 
-// Asignamos una función para que se ejecute con el evento "click" que evita que se ejecute la acción por efecto del clic en el elemento
+// We assign a function to be executed on the "click" event that prevents the default action from being executed when clicking on the element.
 
-root.addEventListener("click", function(event) {
-    event.preventDefault()
-} )
+root.addEventListener("click", function (event) {
+    event.preventDefault();
+});
 
-/* OTRA OPCIÓN:
-
+/* ALTERNATIVE OPTION:
 root.onclick = function(event) {
   event.preventDefault();
-  
-} */
+} 
+*/
 
-// 2. Al hacer click sobre un elemento van a ocurrir varias cosas. Todo depende del tipo de elemento:
+// 2. When clicking on an element, several things will happen. It all depends on the type of element:
+// 2.1 Images: Change the image to one of the GIFs you have in the assets folder with the name magic-*.
 
-// 2.1 Imágenes: Cambia la imagen por uno de los gif que tienes en la carpeta assets con el nombre magic-*.
+const images = document.querySelectorAll("img");
+const gifs = ["/assets/magic-1.gif", "/assets/magic-2.gif", "/assets/magic-3.gif", "/assets/magic-4.gif", "/assets/magic-5.gif", "/assets/magic-6.gif"];
 
-const imágenes = document.querySelectorAll("img");
-const gifs = ["/assets/magic-1.gif", "/assets/magic-2.gif", "/assets/magic-3.gif", "/assets/magic-4.gif", "/assets/magic-5.gif", "/assets/magic-6.gif"]
-
-for (let i = 0; i < imágenes.length; i++) {
-    const imagen = imágenes[i];
-    imagen.onclick = function() {
+for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    image.onclick = function () {
         //let randomGif = gifs[Math.floor(Math.random()*gifs.length)]
-        imagen.setAttribute("src", getRandom(gifs))
-    }
-}
+        image.setAttribute("src", getRandom(gifs));
+    };
+};
 
 /* 
-EXTRA: Opción similar pero usando .forEach
+EXTRA: Similar option but using .forEach
 
-imágenes.forEach(function(imagen) {
-    imagen.onclick = function(event){
+images.forEach(function(image) {
+    image.onclick = function(event){
         let randomGif = gifs[Math.floor(Math.random() * gifs.length)];
-        imagen.setAttribute("src", randomGif);
+        image.setAttribute("src", randomGif);
     }
 })
 */
 
+// 2.2 Paragraphs: Change the text color and background color to any random one.
+// Color palette from coolors.co (exercises 5 and 6): 
 
-// 2.2 Párrafos: Cambia el color del texto y el de fondo por uno cualquiera.
-//Paleta de colores de coolors.co (ejercicio 5 y 6): 
+const colors = ["#1B998B", "EBF5DF", "#8377D1", "#EF767A", "#E8871E"];
 
-const colors = ["#1B998B", "EBF5DF", "#8377D1", "#EF767A", "#E8871E"]
+const paragraphs = document.querySelectorAll("p");
 
-const parrafos = document.querySelectorAll("p");
-
-for (let i = 0; i < parrafos.length; i++) {
-    const parrafo = parrafos[i];
-    parrafo.onclick = function(){
-        parrafo.style.backgroundColor = getRandom(colors) 
-        parrafo.style.color = getRandom(colors)
-    }
-}
-
+for (let i = 0; i < paragraphs.length; i++) {
+    const paragraph = paragraphs[i];
+    paragraph.onclick = function () {
+        paragraph.style.backgroundColor = getRandom(colors);
+        paragraph.style.color = getRandom(colors);
+    };
+};
 
 /* 
-EXTRA: Usando .forEach
+EXTRA: Using .forEach
 
-parrafos.forEach(function(párrafo) {
-    párrafo.onclick = function(event){
-        párrafo.style.color = "yellow";
-        párrafo.style.backgroundColor =  "blue";
+paragraphs.forEach(function(paragraph) {
+    paragraph.onclick = function(event){
+        paragraph.style.color = "yellow";
+        paragraph.style.backgroundColor =  "blue";
     }     
 })
  */
 
 
-// 2.3 Bloques de article o section: Cambia el color de fondo.
+// 2.3 blocks de article o section: Cambia el color de fondo.
 
-const bloques = document.querySelectorAll("section")
+const blocks = document.querySelectorAll("section, article");
 
-for (let i = 0; i < bloques.length; i++) {
-    const bloque = bloques[i];
-    bloque.onclick = function () {
-        bloque.style.backgroundColor = getRandom(colors)
-    }
-    
-}
+for (let i = 0; i < blocks.length; i++) {
+    const block = blocks[i];
+    block.onclick = function () {
+        block.style.backgroundColor = getRandom(colors);
+    };
+};
 
 /*
 EXTRA: Usando .forEach
 
-bloques.forEach(function(sección) {
-    sección.onclick = function(event){
-        sección.style.backgroundColor =  "green";
+blocks.forEach(function(sección) {
+    block.onclick = function(event){
+        block.style.backgroundColor =  "green";
     }
 })
 */
 
-// 3. Cuando el cursor esté sobre alguno de los siguientes elementos, seguir las instrucciones siguientes, y devolver dicho elemento a su estado original cuando salga el cursor.
+// 3. When the cursor is over any of the following elements, follow the instructions below, and return the element to its original state when the cursor leaves.
+// 3.1 Images: Change the image to the abracadabra.gif GIF.
 
-// 3.1 Imágenes: Cambia la imagen por el gif abracadabra.gif.
 
-
-for (let i = 0; i < imágenes.length; i++) {
-    const imagen = imágenes[i];
-    let temp = imagen.src;
-    imagen.onmouseover = function () {
-        imagen.setAttribute("src", "/assets/abracadabra.gif")
+for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    let temp = image.src;
+    image.onmouseover = function () {
+        image.setAttribute("src", "/assets/abracadabra.gif");
     }
-    imagen.onmouseout = function () {
-        imagen.setAttribute("src", temp)
-    }
-}
-
-
-
-/* OPCIÓN .forEach (aun por probar)
-imágenes.forEach(function(imagen) {
-
-    let temp = imagen.src;
-
-    imagen.onmouseover = function(event) {
-        imagen.setAttribute("src", "/assets/abracadabra.gif");
+    image.onmouseout = function () {
+        image.setAttribute("src", temp);
     };
-    imagen.onmouseout = function(event) {
-        imagen.setAttribute("src", temp);
+};
+
+
+
+/* ALTERNATIVE OPTION: .forEach 
+images.forEach(function(image) {
+
+    let temp = image.src;
+
+    image.onmouseover = function(event) {
+        image.setAttribute("src", "/assets/abracadabra.gif");
+    };
+    image.onmouseout = function(event) {
+        image.setAttribute("src", temp);
     }
 }) 
 */
 
+// 3.2 Paragraphs: Change the text color and background color to any random one.
 
-// 3.2 Párrafos: Cambia el color del texto y el de fondo por uno cualquiera.
+for (let i = 0; i < paragraphs.length; i++) {
+    const paragraph = paragraphs[i];
+    let tempParrBackground = paragraph.style.backgroundColor;
+    let tempParrColor = paragraph.style.color;
+    paragraph.onmouseover = function () {
+        paragraph.style.backgroundColor = getRandom(colors);
+        paragraph.style.color = getRandom(colors);
+    };
+    paragraph.onmouseout = function () {
+        paragraph.style.backgroundColor = tempParrBackground;
+        paragraph.style.color = tempParrColor;
+    };
 
-for (let i = 0; i < parrafos.length; i++) {
-    const parrafo = parrafos[i];
-    let tempParrFondo = parrafo.style.backgroundColor;
-    let tempParrColor = parrafo.style.color;
-    parrafo.onmouseover = function(){
-        parrafo.style.backgroundColor = getRandom(colors) 
-        parrafo.style.color = getRandom(colors)
-    }
-    parrafo.onmouseout = function() {
-        parrafo.style.backgroundColor = tempParrFondo 
-        parrafo.style.color = tempParrColor
-    }
-}
+};
 
-// 3.3 Bloques de article o section: Color de fondo distinto al de párrafo.
+// 3.3 Blocks of article or section: Background color different from that of the paragraph.
 
-for (let i = 0; i < bloques.length; i++) {
-    const bloque = bloques[i];
-    let tempBloqueFondo = bloque.style.backgroundColor;
-    bloque.onmouseover = function () {
-        bloque.style.backgroundColor = getRandom(colors)
-    }
-    bloque.onmouseout = function () {
-        bloque.style.backgroundColor = tempBloqueFondo
-    }
-    
+function getDifferentColor(colorsArr, currentColor) {
+    const newArr = [...colorsArr];
+    const index = newArr.indexOf(currentColor);
+    if (index > -1) newArr.splice(index, 1);
+    return getRandom(newArr);
+};
+
+
+for (let i = 0; i < blocks.length; i++) {
+    const block = blocks[i];
+    let tempblockFondo = block.style.backgroundColor;
+    block.onmouseover = function (event) {
+        const targetParagraph = event.target.closes('p');
+        if (targetParagraph) {
+            const paragraphColor = targetParagraph.style.backgroundColor;
+            block.style.backgroundColor = getDifferentColor(colors, paragraphColor);
+        }
+        // block.style.backgroundColor = getRandom(colors)
+    };
+    block.onmouseout = function () {
+        block.style.backgroundColor = tempblockFondo;
+    };
 }
 
 // Premium
-// 4. Crea una función de nombre getRandom que acepte un array con valores y devuelva uno de ellos de manera aleatoria.
+// 4. Create a function named getRandom that accepts an array with values and returns one of them randomly.
 
-function getRandom (arr) {
-    return arr[Math.floor(Math.random()*arr.length)]
+function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const colorsPrueba = ['red', 'blue', 'green']
+const colorsPrueba = ['red', 'blue', 'green'];
 
-console.log(getRandom(colorsPrueba))
+console.log(getRandom(colorsPrueba));
 
-
-// 5. Utiliza la función creada getRandom para utilizar colores aleatorios de una paleta que hayas escogido de coolors.co en los apartados anteriores.
-// 6. Utiliza la función creada getRandom para utilizar gifs aleatorios en los apartados anteriores.
-// 7. Ambos ya realizads arriba
+// 5. Use the created getRandom function to use random colors from a palette you have chosen from coolors.co in the previous sections.
+// 6. Use the created getRandom function to use random GIFs in the previous sections.
+// Both already implemented above.
